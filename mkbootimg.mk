e05b5f08e892f5320ffc3801ba875ef8579a7ff4
XiNGRZ <chenxingyu92@gmail.com>
2017-03-21 00:59:54 +0800

    mkbootimg: Only ramdisk is needed

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,14 +1,4 @@
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) \
-		$(INSTALLED_KERNEL_TARGET) \
-		$(INSTALLED_RAMDISK_TARGET) \
-		$(INSTALLED_RESOURCEIMAGE_TARGET)
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKKRNLIMG) $(INSTALLED_RAMDISK_TARGET)
 	$(call pretty,"Target boot image: $@")
-
-	# Round up the size of ramdisk.img to multiple of 4 bytes
-	truncate -s '%4' $(INSTALLED_RAMDISK_TARGET)
-
-	$(MKBOOTIMG) \
-		--kernel $(INSTALLED_KERNEL_TARGET) \
-		--ramdisk $(INSTALLED_RAMDISK_TARGET) \
-		--second $(INSTALLED_RESOURCEIMAGE_TARGET) \
-		--output $@
+	$(hide) truncate -s '%4' $(INSTALLED_RAMDISK_TARGET)
+	$(hide) $(MKKRNLIMG) $(INSTALLED_RAMDISK_TARGET) $@ > /dev/null
