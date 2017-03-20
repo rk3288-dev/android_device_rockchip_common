9c6f1bc3714041260b4ac5eb35113c0e7931c1d5
XiNGRZ <chenxingyu92@gmail.com>
2017-03-21 03:46:51 +0800

    Make recovery image

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -2,3 +2,13 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKKRNLIMG) $(INSTALLED_RAMDISK_TARGET)
 	$(call pretty,"Target boot image: $@")
 	$(hide) truncate -s '%4' $(INSTALLED_RAMDISK_TARGET)
 	$(hide) $(MKKRNLIMG) $(INSTALLED_RAMDISK_TARGET) $@ > /dev/null
+	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
+
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
+		$(recovery_ramdisk) $(recovery_kernel)
+	$(hide) truncate -s '%4' $(recovery_ramdisk)
+	$(hide) $(MKBOOTIMG) \
+		--kernel $(recovery_kernel) \
+		--ramdisk $(recovery_ramdisk) \
+		--output $@
+	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
