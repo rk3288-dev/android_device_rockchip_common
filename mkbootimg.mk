346546c98e337a5d69eb52dc846b08ced3a6d80e
XiNGRZ <chenxingyu92@gmail.com>
2017-04-12 11:14:53 +0800

    Fix building of recovery image

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -2,13 +2,11 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKKRNLIMG) $(INSTALLED_RAMDISK_TARGET)
 	$(call pretty,"Target boot image: $@")
 	$(hide) truncate -s '%4' $(INSTALLED_RAMDISK_TARGET)
 	$(hide) $(MKKRNLIMG) $(INSTALLED_RAMDISK_TARGET) $@ > /dev/null
-	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE))
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 		$(recovery_ramdisk) $(recovery_kernel)
+	$(call pretty,"Target recovery image: $@")
 	$(hide) truncate -s '%4' $(recovery_ramdisk)
-	$(hide) $(MKBOOTIMG) \
-		--kernel $(recovery_kernel) \
-		--ramdisk $(recovery_ramdisk) \
-		--output $@
-	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
+	$(hide) $(MKKRNLIMG) $(recovery_ramdisk) $@ > /dev/null
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE))
